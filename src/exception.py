49dde17dfb9b4fe918567d066f007dfbda5bbe21
varunsardana <varunsardana2006@gmail.com>
2025-06-26 16:25:10 -0700

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb= error_detail.exc_info()
-    file_name= exc_tb.tb_frame.f_code.co_filename
+    file_name= exc_tb.tb_frame.f_code.co_filenamev
     error_message = (
       "Error occurred in python script name [{0}] "
       "line number [{1}] "
